5017da3a4c4bda98bdd50a2ad365215de3c4b1b0
Ansh Majumdar <137042201+Ansh-Majumdar619@users.noreply.github.com>
2025-01-20 18:07:30 +0530

    Update blacklistToken.model.js

--- backend/models/blacklistToken.model.js
+++ backend/models/blacklistToken.model.js
@@ -1,35 +1,5 @@
-// const mongoose = require('mongoose');
-
-// const blacklistTokenSchema = new mongoose.Schema({
-//     token: {
-//         type: String,
-//         required: true,
-//         unique: true
-//     },
-//     createdAt: {
-//         type: Date,
-//         default: Date.now,
-//         expires: 86400 // 24 hours in seconds
-//     }
-// });
-
-// module.exports = mongoose.model('BlacklistToken', blacklistTokenSchema);
-
-
-
-
-
-
-
-
-
-
-
-
-
 const mongoose = require('mongoose');
 
-// Define the schema
 const blacklistTokenSchema = new mongoose.Schema({
     token: {
         type: String,
@@ -43,8 +13,17 @@ const blacklistTokenSchema = new mongoose.Schema({
     }
 });
 
-// Use existing model if it exists, otherwise create a new one
-const BlacklistToken =
-    mongoose.models.BlacklistToken || mongoose.model('BlacklistToken', blacklistTokenSchema);
+module.exports = mongoose.model('BlacklistToken', blacklistTokenSchema);
 
-module.exports = BlacklistToken;
+
+
+
+
+
+
+
+
+
+
+
+
